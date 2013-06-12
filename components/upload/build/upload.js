cbfb01e4594066f80da3f527f6904b65bd4628de
Geert Pasteels <geert.pasteels@gmail.com>
2013-06-12 18:21:24 +0200

    components: forgot to build upload

--- components/upload/build/upload.js
+++ components/upload/build/upload.js
@@ -112,22 +112,6 @@ var filters = {
 
   },
 
-  fileType: function () {
-
-    return function (path) {
-
-      var ex = path.substring(path.lastIndexOf('.')).toLowerCase();
-
-      if (ex === '.png' || ex === '.jpg' || ex === '.jpeg' || ex === '.gif') {
-        return path;
-      }
-
-      return 'custom/assets/upload/file.png';
-
-    };
-
-  },
-
   fileName: function () {
     
     return function (path) {
