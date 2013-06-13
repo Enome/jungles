fdc572841b9a606b67892020b8703f2a98205840
Geert Pasteels <geert.pasteels@gmail.com>
2013-06-13 20:22:37 +0200

    rest: removed init, bumped major and upped express version

--- rest/index.js
+++ rest/index.js
@@ -1,36 +1,33 @@
-var jungles_rest = {
+var jungles_rest = function (options) {
 
-  init: function (options) {
+  var kwery = require('kwery');
+  var core = require('./core');
+  var express = require('express');
+  var types = require('jungles-types');
 
-    var kwery = require('kwery');
-    var core = require('./core');
-    var express = require('express');
-    var types = require('jungles-types');
+  // App
 
-    // App
+  var app = express();
+  app.use(express.bodyParser());
 
-    var app = express();
-    app.use(express.bodyParser());
+  // Depdencies
 
-    // Depdencies
+  core.data = options.data;
+  core.schemas = { find: kwery.flat.bind(null, options.types) };
+  core.types = types(options.types, options.data);
+  app.core = core;
 
-    core.data = options.data;
-    core.schemas = { find: kwery.flat.bind(null, options.types) };
-    core.types = types(options.types, options.data);
-    app.core = core;
+  // Routes
 
-    // Routes
+  require('./general/routes').init(app);
+  require('./instances/routes').init(app);
+  require('./types/routes').init(app);
 
-    require('./general/routes').init(app);
-    require('./instances/routes').init(app);
-    require('./types/routes').init(app);
+  // Exports
 
-    // Exports
-
-    return app;
-
-  }
+  return app;
 
 };
+
 
 module.exports = jungles_rest;
